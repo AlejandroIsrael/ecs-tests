d18208e47a34c4503c8895af170362b341324eea
Alejandro Israel <alejandro.silva@axen.pro>
2019-05-02 19:24:38 -0500

    Modificación de archivo

--- java/mtax/MTax.java
+++ java/mtax/MTax.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class MTax implements Constant {
+public class MTax {
     
     public MTax(){
         
@@ -12,37 +12,42 @@ public class MTax implements Constant {
         
         List<String> errorList = new ArrayList<>();
         
-//        List<String> taxCategoryList = MInfoTaxCategory.getTaxCategoryStringList();
+//      List<String> taxCategoryList = MInfoTaxCategory.getTaxCategoryStringList();
+        // Eliminar la condición de is null debido a que si es smayor a 0 evidentemente no es nula
         
-        if(xTaxList != null && xTaxList.size() > 0) {
+        if(xTaxList.size() > 0) {
             List<String> validIds = new ArrayList<>();
             int cont = 0;
+            
             for (X_Tax tax : xTaxList) {
+            
                 if(tax.getId() != null){
+            
                     validIds.add(tax.getId().toString());
+            
                 }
-//                if(tax.getAmount() == null) {
-//                    errorList.add("El importe es obligatorio");
-//                }
+//              if(tax.getAmount() == null) {
+//                   errorList.add("El importe es obligatorio");
+//              }
 
                 if(tax.getTax() == null) {
                     errorList.add("El impuesto es obligatorio");
                 }
-//                else if(!taxCategoryList.contains(tax.getTax())) {
+//              else if(!taxCategoryList.contains(tax.getTax())) {
 //                    errorList.add("El impuesto no es un dato valido");
-//                }
+//              }
 
-//                if(tax.isLocal()){
+//              if(tax.isLocal()){
 //                    if(tax.isTrasladado() && tax.getTaxAmount() == null ) {
 //                        errorList.add("El importe es obligatorio");
 //                    }
-//                } 
-//                else {
-//                    if(tax.getTaxAmount() == null ) {
-//                        errorList.add("El importe es obligatorio");
-//                    }
-//                }
-                
+//              } 
+//              else{
+//                  if(tax.getTaxAmount() == null ) {
+//                       errorList.add("El importe es obligatorio");
+//                   }
+//              }
+    
                 if(!tax.isLocal()){
                     cont++;
                 }
@@ -51,13 +56,26 @@ public class MTax implements Constant {
                 errorList.add("Debe de incluir al menos una tasa no local");
             }
             if(validIds.size() > 0){
+
+                /*No se vacía el arreglo validIds en el nuevo arreglo xt debido 
+                a que la conidcion del if verifica que si los arreglos son del mismo 
+                tamaño y se ejecuta la instruccion, eso no es necesario ya que
+                se tiene una if con la condición de que si el arreglo validIds es mayor a 0
+                y si validIds es mayor a 0 quiere decir que xt tambien lo seria y se ejecuta la instruccion
+                y es mejor verificar si validIds es mayor a 0 */
                     
-                    List<X_Tax> xt = TaxsByListId(validIds, false);
+                   /* List<X_Tax> xt = TaxsByListId(validIds, false);
                     if(xt.size() != validIds.size()){
                         errorList.add("Existen datos no guardados previamente");
-                    }else{
+                    }else{*/
                         HashMap<String, X_Tax> map_taxs = new HashMap<String, X_Tax>();
-                        for(X_Tax tax: xt){
+
+                        /*Se eliminó el arreglo xt ya no se usará en el for se reemplazará con 
+                        el arreglo xTaxList y se buscara hacer un ciclo con el for usando la variable Tax
+                        y se igualaría con la variable tax que ya tenía los datos del xTaxList*/
+
+
+                        for(X_Tax tax: /*xt*/xTaxList){
                             map_taxs.put(tax.getId().toString(), tax);
                         }
                         for(int i = 0; i < xTaxList.size(); i++){
@@ -67,7 +85,9 @@ public class MTax implements Constant {
                                                 .getCreated());
                             }
                         }
-                    }
+                    //}      
+            }else{
+                errorList.add("Existen datos no guardados previamente");
             }
         }
 //        else {
